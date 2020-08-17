7f933f3a5ce48141bfb0e0882e77376f77fb849a
BlackUserSide <nesmserg@gmail.com>
2020-08-17 21:24:29 +0300

    bets

--- src/screens/cureentBetScreens/CryptoScreen.js
+++ src/screens/cureentBetScreens/CryptoScreen.js
@@ -1,9 +1,36 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, ImageBackground, FlatList } from "react-native";
+import { MainHeader } from "../../component/MainHeader";
+import { CryptoBets } from "../../component/bets/CrypyoBets";
+
 export const CryptoScreen = () => {
+  const [crypto, setCrypto] = useState([
+    { id: "1", name: "Bitcoin", iconsName: "bitcoin" },
+    { id: "0", name: "Ethereum", iconsName: "ethereum" },
+  ]);
+  const [modal, setModal] = useState(false);
   return (
-    <View>
-      <Text>dsadasd</Text>
-    </View>
+    <ImageBackground
+      source={require("../../../assets/img/bg.png")}
+      style={styles.img}
+    >
+      <MainHeader />
+      <FlatList
+        style={styles.scrollView}
+        keyExtractor={(item) => item.id}
+        data={crypto}
+        renderItem={({ item }) => <CryptoBets data={item} />}
+      />
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  img: {
+    flex: 1,
+  },
+  scrollView: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+});
--- src/screens/cureentBetScreens/CurrencyScreen.js
+++ src/screens/cureentBetScreens/CurrencyScreen.js
@@ -1,9 +1,36 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, ImageBackground, FlatList } from "react-native";
+import { MainHeader } from "../../component/MainHeader";
+import { CurrencyBets } from "../../component/bets/CurrencyBets";
 export const CurrencyScreen = () => {
+  const [currency, setCurrency] = useState([
+    { id: "1", name: "EUR", iconsName: "euro-sign" },
+    { id: "0", name: "RUB", iconsName: "ruble-sign" },
+    { id: "2", name: "UAN", iconsName: "dollar-sign" },
+  ]);
+  const [modal, setModal] = useState(false);
   return (
-    <View>
-      <Text>dsadasd</Text>
-    </View>
+    <ImageBackground
+      source={require("../../../assets/img/bg.png")}
+      style={styles.img}
+    >
+      <MainHeader />
+      <FlatList
+        style={styles.scrollView}
+        keyExtractor={(item) => item.id}
+        data={currency}
+        renderItem={({ item }) => <CurrencyBets data={item} />}
+      />
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  img: {
+    flex: 1,
+  },
+  scrollView: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+});
